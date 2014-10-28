5ac47f3ee3926f930c350dca73edc3ac5175b014
Kevin Liddle <kevin@8thlight.com>
2014-10-27 22:09:16 -0500

    oops

--- spec/battery_staple_spec.rb
+++ spec/battery_staple_spec.rb
@@ -5,6 +5,6 @@ describe BatteryStaple do
   it "reads from actual dictionary and creates some random passwords" do
     password = BatteryStaple.generate(4)
     expect(password.split(" ").size).to eq(4)
-    expect(password.include?("\n")).to be_falsey
+    expect(password.include?("\n")).to be_false
   end
 end
--- spec/randomizer_spec.rb
+++ spec/randomizer_spec.rb
@@ -3,7 +3,7 @@ require 'randomizer'
 describe Randomizer do
   it "uses Kernel#rand" do
 
-    expect(Kernel).to receive(:rand).with(222) { 0 }
+    expect(Kernel).to receive(:rand).with(222).and_return(0)
 
     expect(Randomizer.new.random(222)).to eq(0)
   end
